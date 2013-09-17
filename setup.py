98a7662a8afba184dd724b88731674fb4530c44f
Juho Rutila <juho.rutila@iki.fi>
2013-09-17 16:36:10 +0300

    Version to dev

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "suursuo",
-    version = "0.0.1",
+    version = "dev",
     packages=find_packages(),
     include_package_data=True,
     author = "Juho Rutila",
