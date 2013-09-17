1263fbeb5e549ea105729e96def9f163fb102a6c
Juho Rutila <juho.rutila@iki.fi>
2013-09-17 16:44:34 +0300

    Not zip_safe

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
         'django-apptemplates',
         'django-menus',
     ],
+    zip_safe=False,
     dependency_links = [
       "https://api.github.com/repos/jrutila/django-stables/tarball/multitenant?access_token=30e539ba9491700b201ccbeda82b8cb722c4064c#egg=django-stables-dev",
     ]
