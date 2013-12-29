1c0da44d20296126b393983cd97d158294d0805e
Juho Rutila <juho.rutila@iki.fi>
2013-12-29 11:44:12 +0200

    Initial add of zinnia.

--- setup.py
+++ setup.py
@@ -19,10 +19,14 @@ setup(
         'django-social-auth',
         'django-apptemplates',
         'django-menus',
+        'django-blog-zinnia==0.12.3.1',
+        'PIL',
+        'cmsplugin-zinnia',
     ],
     zip_safe=False,
     dependency_links = [
       "https://api.github.com/repos/jrutila/django-stables/tarball/multitenant?access_token=30e539ba9491700b201ccbeda82b8cb722c4064c#egg=django-stables-dev",
+      "https://api.github.com/repos/jrutila/django-blog-zinnia/tarball/master#egg=django-blog-zinnia-0.12.3.1",
     ]
 
 )
