4ddbedb20385fc20a7338e25d925a6e6ec0b1ad8
Juho Rutila <juho.rutila@iki.fi>
2013-12-30 16:24:31 +0200

    Added package templates.

--- setup.py
+++ setup.py
@@ -1,11 +1,44 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+
+import os
+
+def fullsplit(path, result=None):
+    """
+    Split a pathname into components (the opposite of os.path.join) in a
+    platform-neutral way.
+    """
+    if result is None:
+        result = []
+    head, tail = os.path.split(path)
+    if head == '':
+        return [tail] + result
+    if head == path:
+        return result
+    return fullsplit(head, [tail] + result)
+
+packages, data_files = [], []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+myapp_dir = 'stables'
+
+for dirpath, dirnames, filenames in os.walk(myapp_dir):
+    # Ignore dirnames that start with '.'
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'): del dirnames[i]
+    if '__init__.py' in filenames:
+        packages.append('.'.join(fullsplit(dirpath)))
+    elif filenames:
+        for f in filenames:
+            data_files.append(os.path.join(dirpath.split('/', 1)[1], f))
+
 setup(
     name = "suursuo",
     version = "dev",
     packages=['suursuo'],
-    include_package_data=True,
+    package_data={ 'suursuo': data_files },
     author = "Juho Rutila",
     author_email = "juho.rutila@iki.fi",
     description = "suursuo.fi web app",
@@ -17,7 +50,6 @@ setup(
         'django-cms',
         'django-social-auth',
         'django-apptemplates',
-        'django-menus',
         'django-blog-zinnia',
         'PIL',
         'cmsplugin-zinnia',
