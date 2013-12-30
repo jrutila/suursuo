cb04866ee8f319fbc5ebbda1ee35834f7be12fc8
Juho Rutila <juho.rutila@iki.fi>
2013-12-30 15:52:59 +0200

    Removed django-stables.

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(
     url = "http://suursuo.fi",
     install_requires=[
         'Django==1.5',
-        'django-stables==dev',
         'django-reversion',
         'psycopg2',
         'django-cms',
@@ -25,7 +24,6 @@ setup(
     ],
     zip_safe=False,
     dependency_links = [
-      "https://api.github.com/repos/jrutila/django-stables/tarball/multitenant?access_token=30e539ba9491700b201ccbeda82b8cb722c4064c#egg=django-stables-dev",
       "https://api.github.com/repos/jrutila/django-blog-zinnia/tarball/master#egg=django-blog-zinnia-0.12.3.1",
     ]
 
